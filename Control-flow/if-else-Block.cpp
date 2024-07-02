f72c2df848431aa570fde8e37467d18a35c794f9
Gourav Paul <yashchoudhury22@gmail.com>
2024-07-02 22:55:31 +0530

    added switch case

--- Control-flow/if-else-Block.cpp
+++ Control-flow/if-else-Block.cpp
@@ -2,19 +2,34 @@
 using namespace std;
 
 int main(){
+int age;
 
-int budget;
-cout << "Enter your Budget: "<<endl;
+// int budget;
+// cout << "Enter your Budget: "<<endl;
 
 //input 
-cin >> budget;
+// cin >> budget;
 
-if(budget > 2000000){ // ture wali conditon
-    cout << " you can buy Scorpio " << endl;
+// if(budget > 2000000){ // ture wali conditon
+//     cout << " you can buy Scorpio " << endl;
+// }
+
+// else{ // false wali condition
+//     cout << " you cannot buy Scorpio" <<endl;
+// }
+
+// another statement
+cout << "Enter your age "<<endl;
+
+//input 
+cin >> age;
+
+if (age > 18){
+    cout << "You can drive car" << endl;
 }
 
-else{ // false wali condition
-    cout << " you cannot buy Scorpio" <<endl;
+else {
+    cout << " You cannot drive car" <<endl;
 }
 
  return 0;
