61ec2de6f059bddd38e82cd1a8495c5fb86ba17e
Gourav Paul <yashchoudhury22@gmail.com>
2024-07-02 20:41:00 +0530

    added change file

--- C++/varriable.cpp
+++ C++/varriable.cpp
@@ -3,15 +3,26 @@ using namespace std;
 
 int main(){
 
-// varriable declaration
+// varriable defination
 //   int age = 21;
   // varriable : Named storage merory location
   // marks: memory block ka name varriable
   // datatype varriable = value
 //   cout << "Gaurav's age is " << age << endl;
 
-//declaration
- int age;
+//declaration ('int age' previously declared he)
+//  int age;
+
+// defination
+int age = 21;
+cout << age << endl;
+
+
+// manipulation or updation
+age = 101;
+cout << age << endl;
+
+// Note: Ek single blocks ke undar app same varriable ko re-define nahi kar sakte ho
 
  return 0;
 }
