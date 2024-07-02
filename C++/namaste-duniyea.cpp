4dc09836e99021dc8a55aed214f63db3ccbcbed4
Gourav Paul <yashchoudhury22@gmail.com>
2024-07-02 18:09:48 +0530

    added namaste-duniyea

---
+++ C++/namaste-duniyea.cpp
@@ -0,0 +1,12 @@
+// write down namaste duniyea
+
+#include <iostream>
+using namespace std;
+
+int main(){ // function hai
+
+cout << "Namaste Duniyea" << endl;
+
+    return 0;
+
+} // scope define
