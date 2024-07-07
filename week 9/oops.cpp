fbfff6bed3bcb1b420fda04fcd28cd3c7de5f8d8
Gourav Paul <yashchoudhury22@gmail.com>
2024-07-07 19:17:32 +0530

    added oops file

---
+++ week 9/oops.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+class Student{
+
+    public:
+    //add atttibutes
+    int id;
+    int age;
+    string name;
+    int nos;
+
+
+// Behaviour / Methods / Function
+void study(){
+    cout << " Studing " << endl;
+}
+
+void bunk(){
+
+    cout << " Bunking " << endl;
+}
+
+void sleep(){
+
+cout << "Sleeping " << endl;
+
+}
+
+
+}
+
+
+}
+
+
+int main(){
+ return 0;
+}
