6a1da989a90745b32f56930c2a3014d663e4e6f5
Gourav Paul <yashchoudhury22@gmail.com>
2024-07-07 20:07:28 +0530

    added Parameterised constructor

--- week 9/oops.cpp
+++ week 9/oops.cpp
@@ -12,30 +12,54 @@ class Student{
     string name;
     int nos;
 
+// constructor
+Student(){
+    cout << "Student Defalut ctor called " <<endl;
+}
+//Parameterised constructor 
 
+Student (int id, int age, string name, int nos){
+    cout << " Studnent Parameterised constructor called " << endl;
+}
 // Behaviour / Methods / Function
 void study(){
-    cout << " Studing " << endl;
+    cout << this->name << " Studing " << endl;
 }
 
 void bunk(){
 
-    cout << " Bunking " << endl;
+    cout  << this->name  << " Bunking " << endl;
 }
 
 void sleep(){
 
-cout << "Sleeping " << endl;
+cout << this->name << "Sleeping " << endl;
 
 }
-
-
-}
-
-
-}
-
+// disstructor
+~Student(){
+    cout << "Student Defalut dtor called " <<endl;
+    }
+};
 
 int main(){
+
+Student A;
+A.id  = 1;
+A.name = "Rahul";
+A.nos = 6;
+A.age = 15;
+
+A.study();
+
+
+Student B;
+B.id  = 2;
+B.name = "Rehan";
+B.nos = 5;
+B.age = 15;
+
+B.bunk();
+
  return 0;
 }
